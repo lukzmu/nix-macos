de478bff6b05ad0824b529bda2b4356b7e544bf5
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2025-11-04 18:23:36 +0100

    feat: update keybinds and window size for wezterm

--- configuration/.config/wezterm/wezterm.lua
+++ configuration/.config/wezterm/wezterm.lua
@@ -1,11 +1,18 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.initial_cols = 160
-config.initial_rows = 40
+config.initial_cols = 120
+config.initial_rows = 30
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 config.font_size = 11
+
+local act = wezterm.action
+config.keys = {
+    { key = "z", mods = "CTRL|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+    { key = "x", mods = "CTRL|SHIFT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+    { key = "c", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+}
 
 return config
