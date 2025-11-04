9b7b4cd601baa1e6fea86ba58403fa1f7b92c4d2
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2025-11-04 16:13:00 +0100

    feat: add installation steps

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
-# Install ZSH Plugins
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew packages
+brew install \
+    zsh \
+    starship \
+    neovim \
+    pyenv \
+    mise \
+    lazydocker \
+    lazygit \
+    stow
+
+# Setup ZSH Shell
+chsh -s $(which zsh)
+
+# Install omzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install omzsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
