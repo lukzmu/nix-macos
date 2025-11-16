6d914a17dba94ed983738d6563702654c5e0a990
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2025-11-16 18:51:48 +0100

    chore: add some more theming

--- configuration/.config/wezterm/wezterm.lua
+++ configuration/.config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.initial_cols = 120
-config.initial_rows = 30
+config.initial_cols = 220
+config.initial_rows = 60
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.6
 config.macos_window_background_blur = 10
 config.font_size = 11
 config.send_composed_key_when_left_alt_is_pressed = true
-config.color_scheme = "Catppuccin Mocha"
+config.window_decorations = "RESIZE"
 
 local act = wezterm.action
 config.keys = {
