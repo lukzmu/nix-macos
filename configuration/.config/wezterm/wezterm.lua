32a3346289c9dc5a5961ee6d4a74ca2804133a01
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2025-11-18 09:47:42 +0100

    chore: update theming to be more fancy

--- configuration/.config/wezterm/wezterm.lua
+++ configuration/.config/wezterm/wezterm.lua
@@ -1,15 +1,23 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Window Configuration --
 config.initial_cols = 220
 config.initial_rows = 60
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.6
-config.macos_window_background_blur = 10
 config.font_size = 11
-config.send_composed_key_when_left_alt_is_pressed = true
 config.window_decorations = "RESIZE"
+config.inactive_pane_hsb = {
+  saturation = 0.0,
+  brightness = 0.2,
+}
 
+-- MacOS Configuration --
+config.macos_window_background_blur = 10
+config.send_composed_key_when_left_alt_is_pressed = true
+
+-- Keybinds Configuration --
 local act = wezterm.action
 config.keys = {
     { key = "z", mods = "CTRL|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
