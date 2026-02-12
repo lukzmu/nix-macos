9734bec5d727b0f4fa96734f4f591bbf9bbcfce4
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2026-02-12 07:49:14 +0100

    chore: update configuration variables

--- configuration/.config/wezterm/wezterm.lua
+++ configuration/.config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Window Configuration --
 config.initial_cols = 220
 config.initial_rows = 60
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.8
 config.font_size = 11
 config.window_decorations = "RESIZE"
 config.inactive_pane_hsb = {
-  saturation = 0.0,
-  brightness = 0.2,
+	saturation = 0.0,
+	brightness = 0.2,
 }
 
 -- MacOS Configuration --
@@ -20,9 +20,9 @@ config.send_composed_key_when_left_alt_is_pressed = true
 -- Keybinds Configuration --
 local act = wezterm.action
 config.keys = {
-    { key = "z", mods = "CTRL|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-    { key = "x", mods = "CTRL|SHIFT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-    { key = "c", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "CTRL|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "x", mods = "CTRL|SHIFT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "c", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
 }
 
 return config
