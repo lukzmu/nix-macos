cf06ca5a6a1df186930cf7545702b41ca719e523
Łukasz Żmudziński <lukasz@zmudzinski.sh>
2026-03-05 13:41:05 +0100

    chore: cleanup old configurations

--- configurations/.config/nvim/lua/config/keymap.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
-vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostic" })
--- configurations/.config/nvim/lua/config/window.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_autocmd("VimResized", {
-  callback = function()
-    vim.cmd("wincmd =")
-  end,
-})
