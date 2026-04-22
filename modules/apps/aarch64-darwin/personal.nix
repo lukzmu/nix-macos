7ccf5003d81029ac2207da607293a17343985998
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2026-04-22 14:08:44 +0200

    chore: clean up personal applications

--- modules/apps/aarch64-darwin/personal.nix
+++ modules/apps/aarch64-darwin/personal.nix
@@ -1,18 +1,19 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     brave
+    proton-pass
     signal-desktop
     thunderbird
   ];
   homebrew = {
     enable = true;
+    casks = [
+      "proton-drive" # Not available in Nix packages
+      "proton-mail-bridge" # Doesn't work good with MacOS
+    ];
     masApps = {
       "Amphetamine" = 937984704;
       "Brother iPrint&Scan" = 1193539993;
-      "HomeCam" = 1292995895;
-      "Numbers" = 361304891;
-      "Pages" = 361309726;
-      "uBlock Origin Lite" = 6745342698;
     };
   };
 }
