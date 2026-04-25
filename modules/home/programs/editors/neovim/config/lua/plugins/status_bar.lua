25e5496c1c769d95a2dd97691ea3719c36a5dc5a
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2026-04-25 10:13:26 +0200

    fix: improper catppuccin configuration

--- modules/home/programs/editors/neovim/config/lua/plugins/status_bar.lua
+++ modules/home/programs/editors/neovim/config/lua/plugins/status_bar.lua
@@ -1,10 +1,10 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin' },
     config = function(_)
         require("lualine").setup({
             options = {
-                theme = "catppuccin",
+                theme = "catppuccin-mocha",
             },
         })
     end,
