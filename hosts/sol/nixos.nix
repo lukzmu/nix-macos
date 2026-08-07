9ddfa158b3f9b07cbb54b974f2b02e469a184fda
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2026-08-07 18:23:23 +0200

    feat: add razer device support

--- hosts/sol/nixos.nix
+++ hosts/sol/nixos.nix
@@ -1,9 +1,22 @@
-{...}: {
+{
+  username,
+  pkgs,
+  ...
+}: {
   hardware.nvidia-gpu = {
     enable = true;
     open = true;
     branch = "stable";
   };
+
+  hardware.openrazer = {
+    enable = true;
+    users = [username];
+  };
+
+  environment.systemPackages = with pkgs; [
+    polychromatic
+  ];
 
   zramSwap.enable = true;
 }
